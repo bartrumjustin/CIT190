fa51dc929b053d2c5be4b12cdbf1dd1c9bc8afa2
bartrumjustin <bartrumjustin@gmail.com>
2026-01-17 22:24:25 -0500

    started JS still broken on the eventlisteners CSS media query made to make mobile look better

--- lesson2/js/multipleObjects.js
+++ lesson2/js/multipleObjects.js
@@ -1,2 +1,22 @@
 const date = new Date();
-console.log(date);
+const log = [];
+const log2 = [];
+const pos = document.getElementById("positive");
+const neu = document.getElementById("neutral");
+const neg = document.getElementById("negative");
+var myMood = "";
+var myDetails = "";
+
+function moodCheck() {
+    switch (myMood) {
+        case "positive":
+            log[1] = date;
+            log[2] = myMood;
+            document.getElementById("moodDetail").style.zIndex = "2";
+            break;
+    }
+
+}
+pos.addEventListener("click", function () { myMood = "positive"; moodCheck(); });
+neu.addEventListener("click", function () { myMood = "neutral"; });
+neg.addEventListener("click", function () { myMood = "negative"; })
