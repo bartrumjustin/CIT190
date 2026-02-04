dd7016ac986178b6b42056f4a0fb18b03bc97f33
bartrumjustin <bartrumjustin@gmail.com>
2026-02-04 10:46:35 -0500

    finished task 3 by replacing named function to anon onto the event listener

--- lesson5/js/windchill.js
+++ lesson5/js/windchill.js
@@ -4,7 +4,7 @@ window.onload = getFocus();
 var btn_calc = document.getElementById("calc");
 var btn_reset = document.getElementById("reset");
 
-function windChill() {
+/*function windChill() {
 
 	var wind=parseFloat(document.getElementById("wind").value);
 	var temp = parseFloat(document.getElementById("temp").value);
@@ -24,9 +24,9 @@ function windChill() {
 		document.getElementById("results").innerHTML="<p id='hot'>There is no windchill factor.  Your temp is: " + Math.round(temp) + " degrees</p>";
 	else	
 		document.getElementById("results").innerHTML="<p id='normal'>The windchill factor is: " + Math.round(chill) + " degrees</p>";
-	}	
+	}	*/
 
-}
+
 function getFocus() {
 	console.log("triggered getfocus method");
 	document.getElementById("wind").focus();
@@ -34,7 +34,30 @@ function getFocus() {
 	document.getElementById("results").innerHTML = " ";
 	console.log("clear results and method ends");
 }
-btn_calc.addEventListener("click", windChill);
+//btn_calc.addEventListener("click", windChill);
+btn_calc.addEventListener("click", function() {
+
+		var wind = parseFloat(document.getElementById("wind").value);
+		var temp = parseFloat(document.getElementById("temp").value);
+
+
+
+		if (isNaN(wind) || wind === 0)
+			document.getElementById("results").innerHTML = "<p id='error'>Please enter a number greater than zero for the wind speed!</p>";
+		else if (isNaN(temp))
+			document.getElementById("results").innerHTML = "<p id='error'>Please enter a number for the temperature!</p>";
+		else {
+			var chill = (0.0817 * (3.71 * (Math.pow(wind, 0.5)) + 5.81 - 0.25 * wind) * (temp - 91.4) + 91.4);
+
+			if (chill < 0)
+				document.getElementById("results").innerHTML = "<p id='cold'>The windchill factor is: " + Math.round(chill) + " degrees</p>";
+			else if (chill > 80)
+				document.getElementById("results").innerHTML = "<p id='hot'>There is no windchill factor.  Your temp is: " + Math.round(temp) + " degrees</p>";
+			else
+				document.getElementById("results").innerHTML = "<p id='normal'>The windchill factor is: " + Math.round(chill) + " degrees</p>";
+		}
+})
+
 btn_reset.addEventListener("click", getFocus);
 
 
