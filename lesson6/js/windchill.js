2e0f5d6103f643a7585faef30ae350a43f03fb7a
bartrumjustin <bartrumjustin@gmail.com>
2026-02-13 21:11:00 -0500

    finished Lesson 6 task 1 converting js to jquery

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,8 +1,14 @@
 ﻿
-
-window.onload = getFocus();
-var btn_calc = document.getElementById("calc");
-var btn_reset = document.getElementById("reset");
+$(function () {
+	getFocus();
+});
+$("#reset").on("click", function ()
+{
+	getFocus();
+})
+//window.onload = getFocus();
+//var btn_calc = document.getElementById("calc");
+//var btn_reset = document.getElementById("reset");
 
 /*function windChill() {
 
@@ -26,16 +32,39 @@ var btn_reset = document.getElementById("reset");
 		document.getElementById("results").innerHTML="<p id='normal'>The windchill factor is: " + Math.round(chill) + " degrees</p>";
 	}	*/
 
-
 function getFocus() {
-	console.log("triggered getfocus method");
+	$("#wind").focus();
+	$("#results").html("");
+}
+//function getFocus() {
+	/*console.log("triggered getfocus method");
 	document.getElementById("wind").focus();
 	console.log("Focused")
 	document.getElementById("results").innerHTML = " ";
 	console.log("clear results and method ends");
-}
+}*/
 //btn_calc.addEventListener("click", windChill);
-btn_calc.addEventListener("click", function() {
+$("#calc").on("click", function () {
+	var $wind = eval(parseFloat($("#wind").val()));
+	var $temp = eval(parseFloat($("#temp").val()));
+	
+	if (isNaN($wind) || $wind === 0)
+		$("#results").html("<p id='error'>Please enter a number greater than zero for the wind speed!</p>");
+	else if (isNaN($temp))
+		$("#results").html("<p id='error'>Please enter a number for the temperature!</p>");
+	else {
+		var $chill = (0.0817 * (3.71 * (Math.pow($wind, 0.5)) + 5.81 - 0.25 * $wind) * ($temp - 91.4) + 91.4);
+
+		if ($chill < 0)
+			$("#results").html("<p id='cold'>The windchill factor is: " + Math.round($chill) + " degrees</p>");
+		else if ($chill > 80)
+			$("#results").html("<p id='hot'>There is no windchill factor.  Your temp is: " + Math.round($temp) + " degrees</p>");
+		else
+			$("#results").html("<p id='normal'>The windchill factor is: " + Math.round($chill) + " degrees</p>");
+	}
+
+})
+/*btn_calc.addEventListener("click", function() {
 
 		var wind = parseFloat(document.getElementById("wind").value);
 		var temp = parseFloat(document.getElementById("temp").value);
@@ -58,6 +87,6 @@ btn_calc.addEventListener("click", function() {
 		}
 })
 
-btn_reset.addEventListener("click", getFocus);
+btn_reset.addEventListener("click", getFocus);*/
 
 
