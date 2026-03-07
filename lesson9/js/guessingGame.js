a80fb9ba50bf2dff68bb23b552bf5cc4cc43d462
bartrumjustin <bartrumjustin@gmail.com>
2026-03-06 22:37:52 -0500

    init ex 2

---
+++ lesson9/js/guessingGame.js
@@ -0,0 +1,29 @@
+﻿	var computer = new Number;
+	var tries=new Number(0);
+    
+    document.getElementById("getNumber").addEventListener("click", function(){
+    	tries=0;
+    	document.getElementById("guess").value="";
+    	document.getElementById("tries").value=tries;
+        computer= Math.floor((Math.random() *100) + 1);
+        document.getElementById("comments").value="I have a number and I am waiting for you to guess it";
+    });
+
+    document.getElementById("checkGuess").addEventListener("click", function() {
+        	var guess = new Number(document.getElementById("guess").value);
+      		if (computer==guess){
+      			document.getElementById("comments").value="You guessed correctly - congratulations! It only took " + tries + " tries!";
+      		} else if (computer<guess) {
+        		document.getElementById("comments").value="Your guess is too high, try again!";
+        		tries++;
+        		document.getElementById("tries").value=tries;
+        	}
+        	else {
+        		document.getElementById("comments").value="Your guess is too low, try again!";
+        		tries++;
+           		document.getElementById("tries").value=tries;
+       		}
+        });
+     
+
+
