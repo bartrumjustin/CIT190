9919b260645b366d8a96a7a7d4bae79891bfb712
bartrumjustin <bartrumjustin@gmail.com>
2026-03-21 22:30:58 -0400

    finished

--- lesson10/js/photoviewer.js
+++ lesson10/js/photoviewer.js
@@ -1,7 +1,7 @@
 var request;                         
 var $currentPhoto;                         
 var photoList = {};                      
-var $frame = $('#photoFrame');     
+var $cont = $('#photoCont');     
 var $miniPhotos = $('.minis');           
 
 function crossfade($img) {           
@@ -11,8 +11,9 @@ function crossfade($img) {
     }
 
     $img.css({                         
-        marginLeft: -$img.width() / 2,   
-        marginTop: -$img.height() / 2    
+        
+        
+        position: "absolute"
     });
 
     $img.stop().fadeTo('slow', 1);     
@@ -22,48 +23,47 @@ function crossfade($img) {
 }
 
 $(document).on('click', '.minis', function (e) { 
-    var $img,                               
-        src = this.href;                    
-    request = src;                      
+    var $img,
+        src = $(this).attr('src'),
+        title = $(this).attr('alt');
 
-    e.preventDefault();                     // Stop default link behavior
+    request = src;
+    
 
-    $miniPhotos.removeClass('active');          // Remove active from all thumbs
-    $(this).addClass('active');             // Add active to clicked thumb
+    e.preventDefault();                     
 
-    if (photoList.hasOwnProperty(src)) {        // If cache contains this image
-        if (photoList[src].isLoading === false) { // And if isLoading is false
-            crossfade(photoList[src].$img);         // Call crossfade() function
+    $miniPhotos.removeClass('active');          
+    $(this).addClass('active');
+    console.log(request);
+
+    if (photoList.hasOwnProperty(src)){       
+        
+            crossfade(photoList[src].$img);         
         }
-    } else {                                // Otherwise it is not in cache
-        $img = $('<img/>');                   // Store empty <img/> element in $img
-        photoList[src] = {                        // Store this image in cache
-            $img: $img,                         // Add the path to the image
-            isLoading: true                     // Set isLoading property to false
+    else {                                
+        $img = $('<img/>');                   
+        photoList[src] = {                        
+            $img: $img                                             
         };
 
-        // Next few lines will run when image has loaded but are prepared first
-        $img.on('load', function () {           // When image has loaded
-            $img.hide();                        // Hide it
-            // Remove is-loading class from frame & append new image to it
-            $frame.removeClass('is-loading').append($img);
-            photoList[src].isLoading = false;       // Update isLoading in cache
-            // If still most recently requested image then
+       
+        $img.on('load', function () {          
+            $img.hide();                        
+            
+            $cont.append($img);
             if (request === src) {
-                crossfade($img);                  // Call crossfade() function
-            }                                   // Solves asynchronous loading issue
+                crossfade($img);                  
+            }                                  
         });
 
-        $frame.addClass('is-loading');        // Add is-loading class to frame
-
-        $img.attr({                           // Set attributes on <img> element
-            'src': src,                         // Add src attribute to load image
-            'alt': this.title || ''             // Add title if one was given in link
+        $img.attr({                           
+            'src': src,                         
+            'alt': title || ''            
         });
 
     }
 
 });
 
-// Final line runs once when rest of script has loaded to show the first image
-$('.minis').eq(0).click();                // Simulate click on first thumb
+
+$('.minis').eq(0).click();                
