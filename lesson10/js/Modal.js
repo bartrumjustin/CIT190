642db101e2dda544d018b28bacf38e91d7f025a0
bartrumjustin <bartrumjustin@gmail.com>
2026-03-21 22:45:51 -0400

    adjusted modal to have blur

--- lesson10/js/Modal.js
+++ lesson10/js/Modal.js
@@ -1,5 +1,6 @@
 const modal1 = document.getElementById('Modal1');
-const modal2 = document.getElementById('Modal2')
+const modal2 = document.getElementById('Modal2');
+const menu = document.getElementById('menu');
 
 // Get the button that opens the modal
 var btn1 = document.getElementById("mBtn1");
@@ -13,17 +14,22 @@ var exit2 = document.getElementsByClassName("exit")[1];
 btn1.onclick = function () {
     modal1.style.display = "block";
     modal2.style.display = "none";
+    menu.style.filter = "blur(10px)";
+    
 }
 btn2.onclick = function () {
     modal2.style.display = "block";
     modal1.style.display = "none";
+    menu.style.filter = "blur(10px)";
 }
 
 // When the user clicks on <span> (x), close the modal
 exit1.onclick = function () {
     modal1.style.display = "none";
+    menu.style.filter = "blur(0px)";
 }
 exit2.onclick = function () {
     modal2.style.display = "none";
+    menu.style.filter = "blur(0px)";
 }
 
